72545bb47e803cfecf1a197ce9a5636007bf6fa1
TechyAsylum21 <techyasylumtrainer@gmail.com>
2021-08-29 06:09:56 +0530

    cors

--- src/main/java/com/ta/PocApplication.java
+++ src/main/java/com/ta/PocApplication.java
@@ -3,7 +3,7 @@ package com.ta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-@CrossOrigin(origins="http://localhost:4200")  
+@CrossOrigin(origins="*")  
 @SpringBootApplication
 public class PocApplication {
 
